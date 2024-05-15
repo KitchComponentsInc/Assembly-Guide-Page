4e6176534c828f276e4ea2425a4e4ecfb7851893
Michael McKay <accounts@mykitch.ca>
2024-05-15 08:02:34 -0700

    Transferred data
    To use cache busting techniques, Assembly guides have been moved to a separate file.

---
+++ main.js
@@ -0,0 +1,506 @@
+let assemblyGuides = [
+    /*  ------------------ STANDARD ASSEMBLY GUIDES ------------------ */
+        {
+          category: "standard",
+          name: "BASEBOX",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-PRODUCT_2024.png?v=1712856098",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX.pdf?v=1712769525",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "standard",
+          name: "TALLBOX",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/TallBOX-PRODUCT_2024.png?v=1712856141",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/TallBOX.pdf?v=1712770085",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "standard",
+          name: "WALLBOX",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/WallBOX-PRODUCT_2024.png?v=1712856179",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/WallBOX.pdf?v=1712771618",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "standard",
+          name: "DRAWERBOX",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/DrawerBOX-PRODUCT_2024.png?v=1712856218",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/DrawerBOX_Assembly_Guide.pdf?v=1713366855",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "standard",
+          name: "INNER DRAWERBOX",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Inner_DrawerBOX-PRODUCT_2024.png?v=1712857847",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Inner_DrawerBOX_Assembly_Guide.pdf?v=1713366803",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "standard",
+          name: "OPEN CABINET FOR SEKTION",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Open_Cabinet_15.png?v=1712856321",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Open_Cabinet.pdf?v=1712772269",
+            alt: "Assembly Guide",
+        },
+    
+    
+    
+    
+    
+    
+    
+    /*  ------------------ INSTALLATION GUIDES ------------------ */
+        {
+          category: "installation",
+          name: "APPLIED PANEL",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Applied_Panel_1_5-PRODUCT_2024.png?v=1712857908",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Applied_Panel.pdf?v=1712772272",
+            alt: "Installation Guide",
+        },
+        {
+          category: "installation",
+          name: "FLOATING SHELF",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Floating_Shelf.png?v=1712857962",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Floating_Shelf.pdf?v=1712772275",
+            alt: "Installation Guide",
+        },
+        {
+          category: "installation",
+          name: "TOE KICK - KITCHBOX",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Toe_Kick-PRODUCT_2024.png?v=1714081557",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Toe_Kick_Installation_Guide.pdf?v=1714081396",
+            alt: "Installation Guide",
+        },
+        {
+          category: "installation",
+          name: "TOE KICK - SEKTION",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Toe_Kick.png?v=1714667853",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Toe_Kick_SEKTION_Installation_Guide.pdf?v=1714667990",
+            alt: "Installation Guide",
+        },
+        {
+          category: "installation",
+          name: "FALSE FRONTS FOR KITCHBOX",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/False_Front-PRODUCT_2024.png?v=1715111432",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/False_Fronts_for_KitchBOX.pdf?v=1715111326",
+            alt: "Installation Guide",
+        },
+        /* With QR Code
+        {
+          category: "installation",
+          name: "DOOR CONFIGURATIONS",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-1DO-J_AJAR.png?v=1715367610",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Door_Configurations.pdf?v=1715367240",
+            alt: "Installation Guide",
+        },
+        */
+        {
+          category: "installation",
+          name: "DOOR CONFIGURATIONS",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-1DO-J_AJAR.png?v=1715367610",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/Door_Configurations_v2.pdf?v=1715713570",
+            alt: "Installation Guide",
+        },
+    
+    
+    
+    
+    
+    
+    
+    
+    /*  ------------------ 30" HEIGHT CONFIGURATIONS ------------------ */
+        {
+          category: "30-high",
+          name: "B302DREE",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-2DR-EE_AJAR.png?v=1712856459",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B302DREE.pdf?v=1709316084",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B302DRHB",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-2DR-HB_AJAR.png?v=1712856492",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B302DRHB.pdf?v=1709316132",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B302DRIA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-2DR-IA_AJAR.png?v=1712856525",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B302DRIA.pdf?v=1709316147",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B302DRIEE",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-2DRI-EE_AJAR.png?v=1712856554",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B302DRIEE.pdf?v=1709316157",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B303DRCCC",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-3DR-CCC_AJAR.png?v=1712856634",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B303DRCCC.pdf?v=1709316172",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B303DRDCB",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-3DR-DCB_AJAR.png?v=1712856666",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B303DRDCB.pdf?v=1709316186",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B303DRDDA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-3DR-DDA_AJAR.png?v=1712856698",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B303DRDDA.pdf?v=1709316197",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B303DREBB",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-3DR-EBB_AJAR.png?v=1712856741",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B303DREBB.pdf?v=1709316207",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B303DRECA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-3DR-ECA_AJAR.png?v=1712856858",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B303DRECA.pdf?v=1709316217",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B303DRGAA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-3DR-GAA_AJAR.png?v=1712857242",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B303DRGAA.pdf?v=1709316229",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B303DRICCC",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30-3DR-CCCI_AJAR.png?v=1712857296",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B303DRICCC.pdf?v=1709316239",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "BR30J",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-30_Recyclage-J_AJAR.png?v=1712857329",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BR30J.pdf?v=1709315797",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B305DRCAAA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B305DRCAAAA.png?v=1712857361",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B305DRCAAA.pdf?v=1711470534",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "BBC30DOJ & BBC30DOJS",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BBC-PO_LHS_AJAR.png?v=1712857443",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BBC30DOJ_BBC30DOJS.pdf?v=1712783675",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B302DRGC",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B302DRGC.png?v=1712857477",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B302DRGC.pdf?v=1712675101",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "BS30DODR & BS302DODR",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BS30DODR.png?v=1712947961",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BS30DODR_BS302DODR.pdf?v=1712947894",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B30DODRS & B302DODRS",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B30DODRS.png?v=1713885118",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B30DODRS_B302DODRS.pdf?v=1713884675",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B304DREAAA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B304DREAAA.png?v=1713885517",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B304DREAAA.pdf?v=1713885918",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "B304DRCCAA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B304DRCCAA.png?v=1713886023",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B304DRCCAA.pdf?v=1713885918",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "30-high",
+          name: "BF30DO2ID & BF302DO2ID",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BF30DO2ID.png?v=1713974264",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BF30DO2ID_BF302DO2ID.pdf?v=1713974048",
+            alt: "Assembly Guide",
+        },
+    /* Contains QR CODE
+        {
+          category: "30-high",
+          name: "BBC30DOJ & BBC30DOJS",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BBC-PO_LHS_AJAR.png?v=1712857443",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BBC30DOJ_BBC30DOJS.pdf?v=1715711835",
+            alt: "Assembly Guide",
+        },
+    */
+        {
+          category: "30-high",
+          name: "BBC30DOJ & BBC30DOJS",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BBC-PO_LHS_AJAR.png?v=1712857443",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BBC30DOJ_BBC30DOJS_v2.pdf?v=1715713560",
+            alt: "Assembly Guide",
+        },
+    
+    
+    
+    
+    
+    
+    /*  ------------------ 25" HEIGHT CONFIGURATIONS ------------------ */
+        {
+          category: "25-high",
+          name: "B253DRCCA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B253DRCCA.png?v=1712857520",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B253DRCCA.pdf?v=1709838421",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "25-high",
+          name: "B252DREC",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B252DREC.png?v=1712857555",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B252DREC.pdf?v=1709838421",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "25-high",
+          name: "B252DRGA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B252DRGA.png?v=1712857590",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B252DRGA.pdf?v=1709838421",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "25-high",
+          name: "B253DREAA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B253DREAA.png?v=1712857608",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B253DREAA.pdf?v=1709838421",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "25-high",
+          name: "BS25DRDOCE & BS25DR2DOCE",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BS25DRDOCE.png?v=1712857634",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BS25DRDOCE_BS25DR2DOCE.pdf?v=1709838421",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "25-high",
+          name: "B25DOGAS & B252DOGAS",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B25DOGAS.png?v=1712857675",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B25DOGAS_B252DOGAS.pdf?v=1712252884",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "25-high",
+          name: "BS25DOGA & BS252DOGA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BS25DOGA.png?v=1712947551",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BS25DOGA_BS252DOGA.pdf?v=1712947255",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "25-high",
+          name: "BS252DREC",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BS252DREC.png?v=1712948810",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BS252DREC.pdf?v=1712935305",
+            alt: "Assembly Guide",
+        },
+    
+    
+    
+    
+    
+    
+    
+    /*  ------------------ 20" HEIGHT CONFIGURATIONS ------------------ */
+        {
+          category: "20-high",
+          name: "BS202DRCC",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-20-2DRS-CC_AJAR.png?v=1712858370",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BS202DRCC.pdf?v=1712934608",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "20-high",
+          name: "B202DRCC",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-20-2DR-CC_AJAR.png?v=1712858413",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B202DRCC.pdf?v=1709653936",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "20-high",
+          name: "B202DREA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-20-2DR-EA_AJAR.png?v=1712858448",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B202DREA.pdf?v=1709653979",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "20-high",
+          name: "B20DODREA & B202DODREA",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/BaseBOX-20-DODR-EA_AJAR.png?v=1712858482",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/B20DODREA_B202DODREA.pdf?v=1712931969",
+            alt: "Assembly Guide",
+        },
+        {
+          category: "20-high",
+          name: "WHDO20GS",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/WHDO20GS.png?v=1714078364",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/WHDO10C_WHDO15E_WHDO20GS.pdf?v=1714078259",
+            alt: "Assembly Guide",
+        },
+    
+    
+    
+    
+    
+    
+    
+    /*  ------------------ 15" HEIGHT CONFIGURATIONS ------------------ */
+        {
+          category: "15-high",
+          name: "WHDO15E",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/WHDO15E.png?v=1714079120",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/WHDO10C_WHDO15E_WHDO20GS.pdf?v=1714078259",
+            alt: "Assembly Guide",
+        },
+    
+    
+    
+    
+    
+    
+    
+    /*  ------------------ 10" HEIGHT CONFIGURATIONS ------------------  */
+        {
+          category: "10-high",
+          name: "WHDO10C",
+          imageRef:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/WHDO10C.png?v=1714079140",
+          fileUrl:
+            "https://cdn.shopify.com/s/files/1/0639/8686/0263/files/WHDO10C_WHDO15E_WHDO20GS.pdf?v=1714078259",
+            alt: "Assembly Guide",
+        },
+    
+      ];
